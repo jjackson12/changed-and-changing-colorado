92c40345820666c2d0b0cb97fdf03271d7763b65
jjackson12 <jacob_jackson@brown.edu>
2025-01-03 09:09:56 -0700

    restructuring DataSet class definition

--- data/models.py
+++ data/models.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 import pandas as pd
 from enum import Enum
+from typing import Dict, List
 
 
 class GEOGRAPHY(Enum):
@@ -17,5 +18,21 @@ class DataSet(ABC):
     """Abstract Class defining how datasets for this project will be structured."""
 
     @abstractmethod
-    def collect_data(geography_type: GEOGRAPHY) -> pd.DataFrame:
+    def collect_data(self, geography_type: GEOGRAPHY) -> Dict[str, pd.DataFrame]:
         pass
+
+    @classmethod
+    def create(cls, geography_type: GEOGRAPHY):
+        """Factory method to create and initialize a DataSet instance."""
+        instance = cls()
+        instance.data_tables = instance.collect_data(geography_type)
+        instance.variable_list = list(instance.data_tables.keys())
+        return instance
+
+
+class PopulationData(DataSet):
+
+    def collect_data(self, geography_type: GEOGRAPHY) -> Dict[str, pd.DataFrame]:
+        df_dict = {}
+        # TODO: Collect data here
+        return df_dict
