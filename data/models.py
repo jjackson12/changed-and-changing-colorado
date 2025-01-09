b78201f1b5c41e1ead1cbca712f540c981489a05
jjackson12 <jacob_jackson@brown.edu>
2025-01-09 15:16:30 -0700

    more complete data model, exemplified for collecting Census data

--- data/models.py
+++ data/models.py
@@ -4,6 +4,7 @@ from enum import Enum
 from typing import Dict, List
 from census import Census
 import os
+import requests
 
 
 class GEOGRAPHY(Enum):
@@ -23,6 +24,9 @@ class DataSet(ABC):
     def collect_data(self, geography_type: GEOGRAPHY, args) -> Dict[str, pd.DataFrame]:
         pass
 
+    def get_data(self):
+        return self.data_tables
+
     def __init__(self, geography_type: GEOGRAPHY, **args):
         self.data_tables = self.collect_data(geography_type, args)
         self.variable_list = list(self.data_tables.keys())
@@ -33,12 +37,10 @@ class PopulationData(DataSet):
 
     Args:
         geography_type (GEOGRAPHY): Geographic unit of aggregation
-        args: Dict of additional arguments passed in init for collect_data. For this dataset, this requires year, ACS dataset name, and census api key.
+        args: Dict of additional arguments passed in init for collect_data. For this dataset, this requires year, ACS dataset name, variables (if left blank, will use "*"), and census api key.
     """
 
     def collect_data(self, geography_type: GEOGRAPHY, args) -> Dict[str, pd.DataFrame]:
-
-        df_dict = {}
 
         API_KEY = args["API_KEY"]
         c = Census(API_KEY)
@@ -56,27 +58,23 @@ class PopulationData(DataSet):
         }
         dataset = dataset_mappings.get(args["dataset"], None)
 
+        variables = args["variables"]
+        variable_name_mapping = {}
+        if type(variables) == dict:
+            variable_name_mapping = variables
+            variables = list(variables.keys())
+
         if not dataset:
             raise Exception(
                 f"invalid argument for dataset {args['dataset']}. Must select from these options: {', '.join(dataset_mappings.keys())}"
             )
 
-        # TODO
-        variables = {
-            "B01003_001E": "Total Population",
-            "B02001_002E": "White Population",
-            "B02001_003E": "Black Population",
-            "B02001_005E": "Asian Population",
-            "B19013_001E": "Median Household Income",
-        }
-
         # Colorado FIPS code
         state_fips = "08"
 
         if geography_type == GEOGRAPHY.ZIP5:
-            # TODO: Use Geography var
             results = dataset.state_zipcode(
-                ["NAME"] + list(variables.keys()), state_fips, "*", year=acs_year
+                ["NAME"] + variables, state_fips, "*", year=acs_year
             )
         else:
             if geography_type == GEOGRAPHY.STATE:
@@ -89,20 +87,34 @@ class PopulationData(DataSet):
                 )
         df = pd.DataFrame(results)
 
-        rename_map = {var: desc for var, desc in variables.items()}
-        rename_map.update({"zip code tabulation area": "ZIP Code", "NAME": "Area Name"})
-        df = df.rename(columns=rename_map)
-
-        # Convert numeric columns to proper types
-        for col in variables.values():
-            if col in df.columns:
-                df[col] = pd.to_numeric(df[col], errors="coerce")
+        variable_name_mapping.update(
+            {"zip code tabulation area": "ZIP Code", "NAME": "Area Name"}
+        )
+        df = df.rename(columns=variable_name_mapping)
 
         return {"Population Demos": df}
 
 
+dataset = "acs/acs5/profile"
+year = 2022
+url = f"https://api.census.gov/data/{year}/{dataset}/variables.json"
+
+response = requests.get(url)
+variables = response.json()["variables"]
+
+table_variables = {
+    key: f'{key}_{val["label"]}'
+    for key, val in variables.items()
+    if "PR" not in key and "DP02_" in key
+}
+
 census_api_key = os.environ["CENSUS_API_KEY"]
-census_data_args = {"API_KEY": census_api_key, "year": 2022, "dataset": "acs5"}
+census_data_args = {
+    "API_KEY": census_api_key,
+    "year": 2022,
+    "dataset": "acs5dp",
+    "variables": table_variables,
+}
 pop_data = PopulationData(GEOGRAPHY.ZIP5, **census_data_args)
-pop_data_tbls = pop_data.data_tables
+pop_data_tbls = pop_data.get_data()
 print(pop_data_tbls)
